b65a1056908f60335de5050cd60a3ab8f7bce7b7
KSeeker5 <alexknipper@comcast.net>
2018-02-17 16:16:00 -0500

    Update General_Workstation.bat

--- General_Workstation.bat
+++ General_Workstation.bat
@@ -136,6 +136,23 @@ echo.
 echo.
 echo Changing Color For Visibility
 color 0A
+echo Creating Firewall Port Scripts and File Sharing Scripts
+::Block File Sharing Script
+echo @echo off > C:\Block_File_Sharing.bat
+echo netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No >> C:\Block_File_Sharing.bat
+echo exit >> C:\Block_File_Sharing.bat
+::Enable File Sharing Script
+echo @echo off > C:\Enable_File_Sharing.bat
+echo netsh advfirewall firewall set rule group="File and Printer Sharing new enable=Yes" >> C:\Enable_File_Sharing.bat
+echo exit >> C:\Enable_File_Sharing.bat
+::Share File Script
+echo @echo off > C:\Share_File.bat
+echo net share Fileshare$=C:\Fileshare /grant:everyone,read >> C:\Share_File.bat
+echo exit >> C:\Share_File.bat
+::Stop Sharing File Script
+echo @echo off > C:\Stop_Sharing_File.bat
+echo net share Fileshare$ /delete >> C:\Stop_Sharing_File.bat
+echo exit >> C:\Stop_Sharing_File.bat
 echo Opening Task Manager To Inspect Processes & Services
 start taskmgr
 pause
